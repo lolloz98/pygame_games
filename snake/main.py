dceb613104dc629c18c35d78714f93ffa8aa79e3
lolloz98 <lorenzocarpaneto@yahoo.it>
2023-12-08 19:45:32 +0000

    Place a red rectangle on the screen

--- snake/main.py
+++ snake/main.py
@@ -10,12 +10,19 @@ pygame.display.set_caption('Snake')
 # To control the frame rate:
 clock = pygame.time.Clock()
 
+test_surface_w = 100
+test_surface_h = 200
+test_surface = pygame.Surface((test_surface_w, test_surface_h))
+test_surface.fill('#FF0000')
+
 while True:
     # draw our elements and update everything
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+    screen.blit(test_surface, (200, 100))
     pygame.display.update()
     # this function says that the loop will not run
     # at more than 60 times per second.
