badacae5a1dafcb4431951d50179e5ebc92ffc37
lolloz98 <lorenzocarpaneto@yahoo.it>
2023-12-09 12:26:21 +0000

    Adding collision

--- try/main.py
+++ try/main.py
@@ -25,6 +25,12 @@ snake_vel_x = snake_piece_w
 
 text_surface = test_font.render('Prova', False, '#FFFFFF')
 
+food_piece_w = 20
+food_piece_h = 20
+food_surface = pygame.Surface((food_piece_w, food_piece_h))
+food_surface.fill('#FF0000')
+food_rect = food_surface.get_rect(topleft=(100, 100))
+
 while True:
     # draw our elements and update everything
     for event in pygame.event.get():
@@ -36,9 +42,13 @@ while True:
     snake_pos_x += snake_vel_x
     if snake_pos_x > width + snake_piece_w:
         snake_pos_x = 0
-    snake_rect.left = snake_pos_x
+    snake_rect.x = snake_pos_x
+
+    if snake_rect.colliderect(food_rect) == 1:
+        print("Collision")
     screen.blit(snake_surface, snake_rect)
 
+    screen.blit(food_surface, food_rect)
     screen.blit(text_surface, (700, 30))
 
     pygame.display.update()
@@ -46,5 +56,5 @@ while True:
     # at more than 60 times per second.
     # this set the maximum frame rate
     # (however we are not doing anything for the minimum frame rate)
-    clock.tick(60)
+    clock.tick(30)
 
